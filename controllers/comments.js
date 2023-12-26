73ba0513b0a801b96bfdcd8e27bad263f38b9385
kfblack <kalinblack21@gmail.com>
2023-12-26 16:11:35 -0500

    More CRUD on comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -33,8 +33,54 @@ async function deleteComment(req, res) {
     }
 }
 
+async function edit(req, res) {
+    try {
+        let comment = await Comment.findById(req.params.id);
+        res.render("restaurants/update3", {comment})
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function update(req, res) {
+    try {
+        let review = await Review.findOne({"comments": req.params.id});
+        let restaurant = await Restaurant.findById(review.restaurant);
+        await Comment.findByIdAndUpdate(req.params.id, {description: req.body.description});
+        res.redirect(`/restaurants/${restaurant._id}`);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function updateLike(req, res) {
+    try {
+        let reviews = await Review.findOne({"comments": req.params.id});
+        let restaurant = await Restaurant.findById(reviews.restaurant);
+        await Comment.findByIdAndUpdate(req.params.id, {$inc: {likes: 1}});
+        res.redirect(`/restaurants/${restaurant._id}`)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function updateDislike(req, res) {
+    try {
+        let reviews = await Review.findOne({"comments": req.params.id});
+        let restaurant = await Restaurant.findById(reviews.restaurant);
+        await Comment.findByIdAndUpdate(req.params.id, {$inc: {dislikes: 1}});
+        res.redirect(`/restaurants/${restaurant._id}`)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 module.exports = {
     createComment, 
     new: newComment,
     delete: deleteComment,
+    edit,
+    update, 
+    updateLike,
+    updateDislike
 }
--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,11 @@ const commentsCtrl = require("../controllers/comments");
 const ensureLoggedIn = require("../config/ensureLoggedIn");
 
 router.get("/reviews/:id/new", ensureLoggedIn, commentsCtrl.new);
+router.get("/:id/edit", ensureLoggedIn, commentsCtrl.edit);
 router.post("/reviews/:id", ensureLoggedIn, commentsCtrl.createComment);
 router.delete("/:id", ensureLoggedIn, commentsCtrl.delete);
+router.put("/:id", ensureLoggedIn, commentsCtrl.update);
+router.put("/:id/like", ensureLoggedIn, commentsCtrl.updateLike)
+router.put("/:id/dislike", ensureLoggedIn, commentsCtrl.updateDislike)
 
 module.exports = router;
